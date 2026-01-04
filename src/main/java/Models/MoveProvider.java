c38e038dc8b0d9f3b13bd46776c51b99a47ee9f3
Ahmed_El_Sayyad <elsayyada197@gmail.com>
2026-01-04 14:10:01 +0200

    Add MoveProvider interface to implement 3 class extends from interface according to sessionType(online-offline-computer)

---
+++ src/main/java/Models/MoveProvider.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Models;
+
+/**
+ *
+ * @author Ahmed_El_Sayyad
+ */
+public interface MoveProvider {
+    
+}
