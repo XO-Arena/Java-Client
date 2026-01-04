7f14ad1d6208909479d349ec68a2d42f8f9cec2f
Ahmed_El_Sayyad <elsayyada197@gmail.com>
2026-01-04 10:12:35 +0200

    Implmenet Board Class Model

---
+++ src/main/java/Models/Board.java
@@ -0,0 +1,46 @@
+package Models;
+
+public class Board {
+
+    private int[][] cells;
+
+    public Board() {
+        cells = new int[3][3];
+    }
+
+    public void setCell(int row, int col, int player) {
+
+        cells[row][col] = player;
+    }
+
+    public int getCell(int row, int col) {
+
+        return cells[row][col];
+    }
+
+    public boolean isEmpty(int row, int col) {
+
+        return cells[row][col] == 0;
+    }
+
+    public boolean isFull() {
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (cells[i][j] == 0) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public void reset() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                cells[i][j] = 0;
+            }
+        }
+    }
+
+}
