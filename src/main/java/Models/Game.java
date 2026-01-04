fcd0b6013cdfc3535ff8dfd5b403f8b8462c83b1
Ahmed_El_Sayyad <elsayyada197@gmail.com>
2026-01-04 14:32:48 +0200

    refactor game class implementation

--- src/main/java/Models/Game.java
+++ src/main/java/Models/Game.java
@@ -5,13 +5,11 @@ import Enums.GameResult;
 public class Game {
 
     private Board board;
-    private int currentPlayer;
-    private boolean isGameEnded;
+    private int currentPlayer; // 1 = X , 2 = O
 
     public Game() {
         board = new Board();
         currentPlayer = 1;
-        isGameEnded = false;
     }
 
     public Board getBoard() {
@@ -22,68 +20,56 @@ public class Game {
         return currentPlayer;
     }
 
-    public boolean isGameEnded() {
-        return isGameEnded;
-    }
-
     public boolean playMove(int row, int col) {
-
-        if (isGameEnded) {
-            return false;
-        }
 
         if (!board.isEmpty(row, col)) {
             return false;
         }
 
         board.setCell(row, col, currentPlayer);
-
-        currentPlayer = (currentPlayer == 1) ? 2 : 1;
-
         return true;
     }
 
-    //Game Logic
+    public void switchPlayer() {
+        currentPlayer = (currentPlayer == 1) ? 2 : 1;
+    }
+
     public GameResult checkResult() {
-        // check Rows
+
+        // Rows
         for (int i = 0; i < 3; i++) {
             if (board.getCell(i, 0) != 0
                     && board.getCell(i, 0) == board.getCell(i, 1)
                     && board.getCell(i, 1) == board.getCell(i, 2)) {
-                isGameEnded = true;
                 return board.getCell(i, 0) == 1 ? GameResult.X_WIN : GameResult.O_WIN;
             }
         }
 
-        // Check Columns
+        // Columns
         for (int i = 0; i < 3; i++) {
             if (board.getCell(0, i) != 0
                     && board.getCell(0, i) == board.getCell(1, i)
                     && board.getCell(1, i) == board.getCell(2, i)) {
-                isGameEnded = true;
                 return board.getCell(0, i) == 1 ? GameResult.X_WIN : GameResult.O_WIN;
             }
         }
 
-        // Check Sambosk
+        // Diagonals
         if (board.getCell(1, 1) != 0) {
 
             if (board.getCell(0, 0) == board.getCell(1, 1)
                     && board.getCell(1, 1) == board.getCell(2, 2)) {
-                isGameEnded = true;
                 return board.getCell(1, 1) == 1 ? GameResult.X_WIN : GameResult.O_WIN;
             }
 
             if (board.getCell(0, 2) == board.getCell(1, 1)
                     && board.getCell(1, 1) == board.getCell(2, 0)) {
-                isGameEnded = true;
                 return board.getCell(1, 1) == 1 ? GameResult.X_WIN : GameResult.O_WIN;
             }
         }
 
         // Draw
         if (board.isFull()) {
-            isGameEnded = true;
             return GameResult.DRAW;
         }
 
@@ -93,7 +79,5 @@ public class Game {
     public void reset() {
         board.reset();
         currentPlayer = 1;
-        isGameEnded = false;
     }
-
 }
