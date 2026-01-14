c1c3570e56ce0774e60fc1cdd35e2a798b72e47c
Mohannad El-Sayeh <eng.mohannadelsayeh@gmail.com>
2026-01-14 03:59:30 +0200

    Create Main file and add the shade plugin to create jar

---
+++ src/main/java/com/mycompany/java/client/project/Main.java
@@ -0,0 +1,11 @@
+package com.mycompany.java.client.project;
+
+/**
+ * 
+ * @author mohannad
+ */
+public class Main {
+    public static void main(String[] args) {
+        App.main(args);
+    }
+}
